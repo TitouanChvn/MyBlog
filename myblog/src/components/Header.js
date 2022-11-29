290c384f9c83e92688783ceafc754168640d5e94
CHAUVINEAU Titouan <titouan.chauvineau@telecomnancy.eu>
2022-11-29 17:39:31 +0100

    [+] header with button that leads to Login page

--- myblog/src/components/Header.js
+++ myblog/src/components/Header.js
@@ -1,8 +1,11 @@
 import LoginButton from "./LoginButton"
+import { Link } from "react-router-dom"
+
 
 const Header = () => {
   return (
     <div className="header">
+      <Link className="LinkToHome" to={('/')}>MyBlog</Link>
       header
       <LoginButton/>
     </div>
--- myblog/src/components/LoginButton.js
+++ myblog/src/components/LoginButton.js
@@ -1,11 +1,15 @@
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 
+const LoginButton = (props) => {
+    const navigate = useNavigate();
+    const handleClick = () => navigate('/login');
 
-const LoginButton = () => {
-  return (
-    <button>
-      Log in
-    </button>
-  )
-}
+    return (
+        <button  className="ButtonLogin" onClick={handleClick}>
+        Log in
+        </button>
+        )
+    }
 
 export default LoginButton
