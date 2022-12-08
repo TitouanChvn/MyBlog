af98e24497ecfc8d0f628bdd467c182692fdecb6
CHAUVINEAU Titouan <titouan.chauvineau@telecomnancy.eu>
2022-12-08 14:44:14 +0100

    [+] added a database accessed by sqlite in the backend

--- myblog/backend/server.py
+++ myblog/backend/server.py
@@ -1,48 +1,23 @@
 from flask import Flask
 from flask_cors import CORS,cross_origin
+import sqlite3
+
 
 app = Flask(__name__)
 cors = CORS(app)
 
+
+
 @app.route('/feeddata', methods=['GET','POST'])
 @cross_origin()
-def feed_data():
+def data_from_database():
+    conn = sqlite3.connect('backend\MyBlogdb.db')
+    list = conn.execute('SELECT * FROM Feedcontent')
     return {
-  "Feed_content":[
-    {
-      "id": 1,
-      "title": "Titre1",
-      "author": "Titoutan",
-      "content": "Ici le contenu du post",
-      "image": "/images/imagepublic1.jpg",
-      "date": "2020-01-01"
-    },
-    {
-      "id": 2,
-      "title": "Titre2",
-      "author": "Titoutan",
-      "content": "Autre contenu de autre post",
-      "image": "/images/imagepublic1.jpg",
-      "date": "2020-01-01"
-    },
-    {
-      "id": 3,
-      "title": "Titre3",
-      "author": "Editor2",
-      "content": "Contenu d'un autre editeur",
-      "image": "/images/imagepublic1.jpg",
-        "date": "2020-01-01"
-    },
-    {
-      "id": 3,
-      "title": "Encore un autre élément",
-      "author": "De Titoutan",
-      "content": " BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU BEAUCOUP DE CONTENU ",
-      "image": "/images/imagepublic1.jpg",
-        "date": "2020-01-01"
+        'Feed_content': [dict(id=row[0], title=row[1], author=row[2], content=row[3], image=row[4], date=row[5]) for row in list.fetchall()]
     }
-  ]
-}
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
